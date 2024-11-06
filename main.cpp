b33935ba25259951a3e79dd37debda938489fa74
Yagho Grossi <yaghogrossi@gmail.com>
2024-11-06 16:51:57 -0500

    ...

--- main.cpp
+++ main.cpp
@@ -3,29 +3,90 @@
 
 using namespace std;
 
-struct Node
-{
-    string data;
-    Node* next;
-};
 
 class LinkedList
 {
     private:
+        struct Node
+        {
+            string data;
+            Node* next;
+        };
         Node* first;
     public:
         LinkedList()
             :first(nullptr){}
         ~LinkedList(){ //since we have the destructor, we need the big three in the Linked List
-            for (Node* i=first; i!=nullptr; i=i->next){
-                delete i;
-            }
+            clear();
         }
         LinkedList(const LinkedList& other){
-            first = other.first;
+            LinkedList();
+            copy(other);
         }
-        LinkedList& 
-        bool empty() const {return !first;} //could do first == nullptr
+        LinkedList& operator=(const LinkedList& rhs){
+            if (this == &rhs) return *this;
+            clear();
+            copy(rhs);
+        }
+        //clears ALL elements of the linked list 
+        void clear(){
+            while(!empty()){
+                pop(); //much easier 
+            }
+        }
+        //require empty
+        //we assign everybody to 
+        void copy(const LinkedList& src){
+            assert(empty());
+            Node* last=nullptr;
+            for(Node* p=src.first; p!=nullptr; p=p->next){
+                Node* q = new Node;
+                q->data = p->data;
+                q->next = nullptr;
+                if(last==nullptr){
+                    first = last = q;
+                } else{
+                    last->next = q;
+                    last = q;
+                }
+            }
+        }
+        //this will be LinkedList::Iterator, always like this the iterator type,
+        //because the Linked List uses the begin() and end(), which require access to the 
+        //Iterator's inner stuff. 
+        class Iterator{ //we put it inside here to give it access to the private stuff
+        private:
+            friend class LinkedList;
+            //private constructor, since Node itself is private. 
+            //The only one using this is the List
+            Iterator(Node* np)
+                :node_ptr(np){}
+            
+            Node* node_ptr;
+        public:
+            Iterator()
+                : node_ptr(nullptr) {}
+            
+            string& operator*() const{
+                assert(node_ptr);
+                return node_ptr->data;
+            }
+
+            Iterator& operator++(){
+                assert(node_ptr);
+                node_ptr = node_ptr->next;
+                return *this;
+            }
+
+            bool operator==(const Iterator& rhs) const{
+                return node_ptr == rhs.node_ptr;
+            }
+        };
+
+        Iterator begin() { return Iterator(first); }
+        Iterator end() { return Iterator(); }
+
+        bool empty() const { return !first; } //could do first == nullptr
         //R: list not empty
         //Eff: returns a reference to the item at the front of the list
         string& peek() const{
@@ -44,6 +105,7 @@ class LinkedList
         //R: list not empty
         //Eff: removes and returns the item at the front of the list
         string pop(){
+            assert(!empty());
             string removed_string = first->data;
             Node* temp = first->next;
             delete first;
@@ -60,3 +122,4 @@ class LinkedList
             }
         }
 };
+
